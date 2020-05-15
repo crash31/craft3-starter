3206479814ba344424dfbf7313afe45fb8c993ac
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2020-05-15 15:44:19 -0400

    updating plugin packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    theme: {
-      extend: {}
-    },
-    variants: {},
-    plugins: []
-}
+	// Overwrites theme elements
+	theme: {
+		// Adds to theme elements without overwriting them
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
